32c90eafdfb0d692957f7c22f5b0ccc2667c00ad
Zhuyb <919220608@qq.com>
2019-03-23 23:32:48 +0800

    默认参数可选参数

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
 var arr = ['string', 'wts'];
 var a = [1, 2, 3];
 var b = [1, 2, 3, 4];
-console.log(b[1]);
+// console.log(b[1])
 var str = 'ds';
 var strt = str;
-console.log(strt);
+// console.log(strt)
 var my_tuple = [1, 's'];
+// 函数
+// function add(a:number,b:number):number {
+//     return a+b
+// }
+// console.log(add(1,3))
+var arrow_add = function (a, b) {
+    return a + b;
+};
+//console.log(arrow_add(1,4).toFixed(2))
+var a_a_void = function (a, b) {
+    if (b === void 0) { b = 10; }
+    if (b) {
+        console.log(a + b);
+    }
+    else {
+        console.log(a);
+    }
+};
+console.log(a_a_void(10, 20));
--- index.ts
+++ index.ts
@@ -1,1 +1,32 @@
-var fn = () => 'response'
+let arr:string[] = ['string','wts']
+let a:number[] = [1,2,3];
+let b:Array<number> = [1,2,3,4]
+// console.log(b[1])
+
+let str:string = 'ds';
+let strt = str;
+// console.log(strt)
+
+let my_tuple:[number,string] = [1,'s'];
+
+// 函数
+// function add(a:number,b:number):number {
+//     return a+b
+// }
+// console.log(add(1,3))
+
+let arrow_add = (a:number,b:number):number => {
+    return a+b
+}
+//console.log(arrow_add(1,4).toFixed(2))
+
+
+let a_a_void = (a:number,b = 10):void => {
+
+    if(b){
+        console.log(a+b)
+    }else {
+        console.log(a)
+    }
+};
+console.log(a_a_void(10,20))
