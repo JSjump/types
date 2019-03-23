45f2b9e00aac22a9d90b4a789e35f3409223c20b
Zhuyb <919220608@qq.com>
2019-03-23 23:09:48 +0800

    数组元组

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-var fn = function () { return 'response'; };
+var arr = ['string', 'wts'];
+var a = [1, 2, 3];
+var b = [1, 2, 3, 4];
+console.log(b[1]);
+var str = 'ds';
+var strt = str;
+console.log(strt);
+var my_tuple = [1, 's'];
