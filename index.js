2dba7a6add2e37a00c2e57bc10b56c363cfe3032
Zhuyb <919220608@qq.com>
2019-03-27 21:34:43 +0800

    interface接口可以继承多个接口,class贯彻implement

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
-// let a:any = 'sssss';
-// let b = (<string>a).substring(0,3)
-// console.log(a)
-var peo = {};
-peo.name = '123';
-peo.age = 12;
-var stu = {};
-stu.name = 'dfs';
-var ss = {
-    name: '231',
-    age: 12
-};
+// let st:PerStu = {name:'ds'};
+var CperStu = /** @class */ (function () {
+    function CperStu() {
+    }
+    return CperStu;
+}());
--- index.ts
+++ index.ts
@@ -1,19 +1,18 @@
-// let a:any = 'sssss';
-// let b = (<string>a).substring(0,3)
-// console.log(a)
-
 interface Person {
+    name:string
+}
+interface Student{
+    age:number
+}
+
+interface PerStu extends Person,Student {
+  old:number
+}
+// let st:PerStu = {name:'ds'};
+
+class CperStu implements Person,PerStu{
     name:string;
+    old:number;
     age:number;
 }
-let peo=<Person>{};
-peo.name = '123';
-peo.age = 12;
 
-let stu = {} as Person;
-stu.name = 'dfs';
-
-let ss:Person = {
-    name : '231',
-    age: 12
-};
