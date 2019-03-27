3d98e7bf68d44c00fbc4368c2f9a8426d4e51be9
Zhuyb <919220608@qq.com>
2019-03-27 22:27:20 +0800

    update上次commit

--- index.ts
+++ index.ts
@@ -9,6 +9,6 @@ interface Student extends Person{
      old:number;
 }
 
-class SuperStudent extends Person implements Student{
+class SuperStudent extends Person implements Student{ //implements 贯彻接口Student,Student接口是什么样，怎么操作的，执行贯彻的也得怎么操作
  old:number
 }
